89fa055a075c392c607a59fc2a5bfc655972f29c
nick <root@DESKTOP-LKF3LTN.localdomain>
2020-02-27 15:34:51 -0500

    profile page, kinda ugly

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -4,6 +4,7 @@ import styles from "./profile.module.css"
 import Typography from '@material-ui/core/Typography'
 import Grid from '../../containers/mui/grid'
 import Button from '../../containers/mui/button'
+import Link from '@material-ui/core/Link'
 
 class Profile extends React.Component {
     state = {
@@ -13,53 +14,84 @@ class Profile extends React.Component {
     render() {
         return (
             <React.Fragment>
-            <Container>
-                <div className={styles.user_info}>
+            <Container className={styles.page_content}>
+                <Container>
+                    <Container className={styles.user_info}>
 
+                        <img 
+                        alt=''
+                        className={styles.user_ProfilePic} 
+                        src={require('../../static/Profile/my_icon.jpg')}
+                        />
+                        <Grid>
+                        <Typography variant="h5">Name: {'Nick1225'}</Typography>
+                        <Typography variant="h5">Address: {'50 Charles St E'}</Typography>
+                        <Typography variant="h5">Email: {'N/A'}</Typography>
+                        <Typography variant="h5">Phone: {'N/A'}</Typography>
+                        <Typography variant="h5">FaceBook: {'N/A'}</Typography>
+                        </Grid>
+                    </Container>
                     <img 
-                      alt=''
-                      className={styles.user_ProfilePic} 
-                      src={require('../../static/Profile/my_icon.jpg')}
-                    />
-                    <Grid>
-                    <Typography variant="h5">Name: {'Nick1225'}</Typography>
-                    <Typography variant="h5">Address: {'50 Charles St E'}</Typography>
-                    <Typography variant="h5">Email: {'N/A'}</Typography>
-                    <Typography variant="h5">Phone: {'N/A'}</Typography>
-                    <Typography variant="h5">FaceBook: {'N/A'}</Typography>
-                    </Grid>
-                </div>
-                <img className={styles.user_banner} src={require('../../static/spanner/Essential-Books.jpg')} alt='' />
-                <div className={styles.recommended_books}>
-                    <p>
-                        <img className={styles.book} src={require('../../static/spanner/Essential-Books.jpg')} alt='' />
-                    </p>
-
-
-                </div>
-                <img alt='' className={styles.user_banner} src={require('../../static/spanner/Essential-Books.jpg')}/>
-            
-            </Container>
-            <Grid className={styles.recommended_books}>
-                <Grid  className={styles.book}>
-                    <Button to='placeholder'>
-                        <img alt='' src={require('../../static/Profile/my_icon.jpg')} className={styles.book_cover}/>
-                    </Button>
-                    <Typography variant="p">
-                        "Harrypotter"
-                    </Typography>
-                </Grid>
-
-                <Grid  className={styles.book}>
-                    <Button to='placeholder'>
-                        <img alt='' src={require('../../static/spanner/Essential-Books.jpg')} className={styles.book_cover}/>
-                    </Button>
-                    <Typography variant="p">
-                        "NotHarrypotter"
-                    </Typography>
-                </Grid>
+                    className={styles.user_banner} 
+                    src={require('../../static/spanner/Essential-Books.jpg')} 
+                    alt='' />
+                    
                 
-            </Grid>
+                </Container>
+
+        
+                <Grid className={styles.comments} container spacing={1}>
+                    <Typography variant='h3'> My comments </Typography>
+                    <Grid container item xs={12} spacing={3}>
+                        <Container className={styles.comment}>            
+                            <Container>
+                                <img 
+                                className={styles.book_cover} 
+                                src={require('../../static/spanner/Essential-Books.jpg')}
+                                alt=''
+                                />                        
+                                <Link className={styles.comment_title}>
+                                    <Typography variant='h4' className={styles.comment_title}> Harry Potter </Typography>
+                                </Link>
+                                <Typography variant='h5' className={styles.comment_content}> Hmmmm... </Typography>   
+                            </Container>
+                        </Container>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Container className={styles.comment}>            
+                            <Container>
+                                <img 
+                                className={styles.book_cover} 
+                                src={require('../../static/spanner/Essential-Books.jpg')}
+                                alt=''
+                                />
+                                <Link className={styles.comment_title}>
+                                    <Typography variant='h4' className={styles.comment_title}> Harry Potter 2 </Typography>
+                                </Link>
+                                <Typography variant='h5' className={styles.comment_content}> Harry Potter 2 is so good! </Typography>   
+                            </Container>
+                        </Container>
+                    </Grid>
+                    <Grid container item xs={12} spacing={3}>
+                        <Container className={styles.comment}>            
+                            <Container>
+                                <img 
+                                className={styles.book_cover} 
+                                src={require('../../static/spanner/Essential-Books.jpg')}
+                                alt=''
+                                />
+                                <Link className={styles.comment_title}>
+                                    <Typography variant='h4' className={styles.comment_title}> Harry Potter 3 </Typography>
+                                </Link>
+                                <Typography variant='h5' className={styles.comment_content}> I think Harry Potter 3 is the best! </Typography>
+                            </Container>
+                        </Container>
+                    </Grid>
+                
+                    
+                
+                </Grid>
+            </Container>
             </React.Fragment>
         )
     }
