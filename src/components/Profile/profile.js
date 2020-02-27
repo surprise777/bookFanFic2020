2a130559e956a2839133c1e38a311bc4b819dd06
nick <root@DESKTOP-LKF3LTN.localdomain>
2020-02-27 02:46:15 -0500

    orz

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,6 +1,9 @@
 import React from "react"
 import Container from "../../containers/mui/container"
 import styles from "./profile.module.css"
+import Typography from '@material-ui/core/Typography'
+import Grid from '../../containers/mui/grid'
+import Button from '../../containers/mui/button'
 
 class Profile extends React.Component {
     state = {
@@ -14,30 +17,43 @@ class Profile extends React.Component {
                 <div className={styles.user_info}>
 
                     <img 
+                      alt=''
                       className={styles.user_ProfilePic} 
                       src={require('./static/my_icon.jpg')}
                     />
-                    <h3>Name: {'Nick1225'}</h3>
-                    <h3>Address: {'50 Charles St E'}</h3>
-                    <h3>Email: {'N/A'}</h3>
-                    <h3>Phone: {'N/A'}</h3>
-                    <h3>FaceBook: {'N/A'}</h3>
+                    <Grid>
+                    <Typography variant="h5">Name: {'Nick1225'}</Typography>
+                    <Typography variant="h5">Address: {'50 Charles St E'}</Typography>
+                    <Typography variant="h5">Email: {'N/A'}</Typography>
+                    <Typography variant="h5">Phone: {'N/A'}</Typography>
+                    <Typography variant="h5">FaceBook: {'N/A'}</Typography>
+                    </Grid>
                 </div>
 
 
-
-
-
             </Container>
-            <img className={styles.user_banner} src={require('./static/uoft_banner.jpeg')}/>
-            <div className={styles.recommended_books}>
-                <p>
-                    <img className={styles.book} src={require('./static/uoft_banner.jpeg')}/>
-                </p>
-                
-
-            </div>
+            <img alt='' className={styles.user_banner} src={require('./static/uoft_banner.jpeg')}/>
             
+            <Grid className={styles.recommended_books}>
+                <Grid  className={styles.book}>
+                    <Button to='placeholder'>
+                        <img alt='' src={require('./static/my_icon.jpg')} className={styles.book_cover}/>
+                    </Button>
+                    <Typography variant="p">
+                        "Harrypotter"
+                    </Typography>
+                </Grid>
+
+                <Grid  className={styles.book}>
+                    <Button to='placeholder'>
+                        <img alt='' src={require('./static/uoft_banner.jpeg')} className={styles.book_cover}/>
+                    </Button>
+                    <Typography variant="p">
+                        "NotHarrypotter"
+                    </Typography>
+                </Grid>
+                
+            </Grid>
             
             </div>
         )
