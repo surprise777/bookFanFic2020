215d377170abc3be5417d1aa68e6fa3f037f55d8
Marvel.Z <583675307@qq.com>
2020-02-26 21:32:12 -0500

    use the container

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,15 +2,15 @@ import React from "react";
 
 import "./home.module.css"
 import Navigationbar from "../Navbar/navBar"
-
+import Container from "../../containers/mui/container"
 class Home extends React.Component{
 
     render(){
         const {login_status} = this.props.state;
         return(
-            <div>
+            <Container>
                 <Navigationbar login_status={login_status}/> 
-            </div>
+            </Container>
         )
     }
 }
