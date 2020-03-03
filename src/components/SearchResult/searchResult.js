27811e3d57b13c38f5f85a40cec2157d228673de
Marvel.Z <583675307@qq.com>
2020-03-02 20:34:48 -0500

    complete search result page

---
+++ src/components/SearchResult/searchResult.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Container from '../../containers/mui/container';
+import styles from "./searchResult.module.css"
+import {BookList} from '../../contents/recommendBooks';
+import BookCard from '../BookCard/bookCard';
+import Grid from '../../containers/mui/grid';
+import { Box } from "@material-ui/core";
+import Review from '../Review/review';
+import SectionHeader from '../SectionHeader/sectionHeader';
+import Footer from '../Footer/footer';
+import comments from '../../contents/commentCollection';
+import SideBar from '../SideBar/sideBar';
+
+class Search extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            searchTitle: "Harry Potter",
+            searchBook: BookList,
+            searchReview: Review,
+            searchComment: comments,
+        }
+    
+
+    }
+    render(){
+        const reg = new RegExp(this.state.searchTitle)
+        return(
+            <Container maxWidth={false} disableGutters={true}>
+                <Box className={styles.padding_bottom}>
+                <Container maxWidth='lg'>
+                <Grid container>
+                    <Grid item xs={12} md={8}>
+                        <Box px={6} pt={4}>
+                            <SectionHeader headerText={"Books with '"+ this.state.searchTitle+ "'"} />
+                            <Grid  fullWidth
+                                        container
+                                        justify="space-between"
+                                        alignItems="center">
+                                {BookList.filter(bk => reg.test(bk.title)).map((book, index) => (<Grid container item xs={12} sm={6} md={4} lg={3} className={styles.cardLayout} key={index} justify="center">
+                                    <BookCard media={book.image} title={book.title} book={book} />
+                                </Grid>) )}
+                            </Grid>
+                        </Box>
+
+                        <Box px={6} pt={4}>
+                            <SectionHeader headerText={"Comments with '" + this.state.searchTitle+ "'" } />
+                            <Grid  fullWidth
+                                        container
+                                        justify="space-between"
+                                        alignItems="center">
+                                {BookList.filter(bk => reg.test(bk.title)).map((book, index) => (<Grid container item xs={12} sm={6} md={4} lg={3} className={styles.cardLayout} key={index} justify="center">
+                                    <BookCard media={book.image} title={book.title} book={book} />
+                                </Grid>) )}
+                            </Grid>
+                        </Box>
+
+                        <Box px={6} pt={4} className={styles.overWidth}>
+                            <SectionHeader headerText={"Reviews with '"+ this.state.searchTitle+ "'"} />
+                            <Review src={require("../../static/book-cover/harrypotter.jpg")} title='Welcome to Hogwarts' author='Beyond the sky' rating={4}/>
+                        </Box>
+                    </Grid>
+                    <SideBar/>
+                </Grid>
+                </Container>
+                </Box>
+                <Footer/>
+            </Container>
+        )
+    }
+}
+
+export default Search;
--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,7 @@ import Profile from './components/Profile/profile';
 import AdminProfile from './components/AdminProfile/adminProfile';
 import BookDetail from './components/BookDetails/bookDetails';
 import BookReview from './components/BookReview/bookReview';
-
+import Search from './components/SearchResult/searchResult';
 
 class Routes extends React.Component {
     state = {
@@ -36,6 +36,8 @@ class Routes extends React.Component {
                             (<Layout pageName={RoutesMap.BookDetail.name}><BookDetail state={this.state} book={this.state.book}/></Layout>)}/>
                 <Route exact path={RoutesMap.BookReview.path} render={() =>
                             (<Layout pageName={RoutesMap.BookReview.name}><BookReview state={this.state}/></Layout>)}/>
+                <Route exact path={RoutesMap.SearchResult.path} render={() =>
+                            (<Layout pageName={RoutesMap.SearchResult.name}><Search state={this.state}/></Layout>)}/>
               </Switch>
             </BrowserRouter>
         )
