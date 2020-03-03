99073a03af09b194910135d4a0c3015e63356de2
Marvel.Z <583675307@qq.com>
2020-03-03 01:18:39 -0500

    update login interaction

---
+++ src/contents/dummyData/user.js
@@ -0,0 +1,19 @@
+const users = [
+    {
+        userName: "A penguin",
+        password: "123456",
+        acctType: "u",
+    },
+    {
+        userName: "Killer Bee",
+        password: "123456",
+        acctType: "u",
+    },
+    {
+        userName: "Hina",
+        password: "123456",
+        acctType: "a",
+    },
+]
+
+export default users;
