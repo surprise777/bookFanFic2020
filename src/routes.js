13f91d0d25332d0b85230d2d2743e5a73f82ae78
Marvel.Z <583675307@qq.com>
2020-03-02 20:52:45 -0500

    start building data states

--- src/routes.js
+++ src/routes.js
@@ -12,32 +12,41 @@ import BookReview from './components/BookReview/bookReview';
 import Search from './components/SearchResult/searchResult';
 
 class Routes extends React.Component {
+    constructor() {
+    super()
+    // The handler is passed down as a prop method for children to manipulate parent state. Here, the handler is bound to App.js context.
+    this.handler = this.handler.bind(this)
+    }
     state = {
       login_status: false,
       book: '',
     }
     
+    // You pass the handler the full state to update each time. This means the child needs to call the current state as an object into a temporary variable, make changes, and then pass in the variable as the update here. See an example in pages/concerns.js:14 for what that looks like. For us, here. it just means we pass the state as a property down the component tree.
+    handler(update) {
+      this.setState(update)
+  }
     render() {
         return (
 
             <BrowserRouter>
               <Switch>
                 <Route exact path={RoutesMap.Home.path} render={() => 
-                            (<Layout pageName={RoutesMap.Home.name}><Home state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.Home.name}><Home handler={this.handler}  state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.Login.path} render={() =>
-                            (<Layout pageName={RoutesMap.Login.name}><Login state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.Login.name}><Login handler={this.handler}  state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.Signup.path} render={() =>
-                            (<Layout pageName={RoutesMap.Signup.name}><Signup state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.Signup.name}><Signup handler={this.handler} state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.Profile.path} render={() =>
-                            (<Layout pageName={RoutesMap.Profile.name}><Profile state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.Profile.name}><Profile handler={this.handler} state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.AdminProfile.path} render={() =>
-                            (<Layout pageName={RoutesMap.AdminProfile.name}><AdminProfile state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.AdminProfile.name}><AdminProfile handler={this.handler} state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.BookDetail.path} render={() =>
-                            (<Layout pageName={RoutesMap.BookDetail.name}><BookDetail state={this.state} book={this.state.book}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.BookDetail.name}><BookDetail handler={this.handler} state={this.state} book={this.state.book}/></Layout>)}/>
                 <Route exact path={RoutesMap.BookReview.path} render={() =>
-                            (<Layout pageName={RoutesMap.BookReview.name}><BookReview state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.BookReview.name}><BookReview handler={this.handler} state={this.state}/></Layout>)}/>
                 <Route exact path={RoutesMap.SearchResult.path} render={() =>
-                            (<Layout pageName={RoutesMap.SearchResult.name}><Search state={this.state}/></Layout>)}/>
+                            (<Layout pageName={RoutesMap.SearchResult.name}><Search handler={this.handler} state={this.state}/></Layout>)}/>
               </Switch>
             </BrowserRouter>
         )
