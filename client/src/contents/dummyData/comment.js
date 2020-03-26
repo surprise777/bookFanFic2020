1abcc44480ffe10862178acfe817005cc8fb8f30
Marvel.Z <583675307@qq.com>
2020-03-26 17:46:07 -0400

    fix the comment data

--- client/src/contents/dummyData/comment.js
+++ client/src/contents/dummyData/comment.js
@@ -4,24 +4,32 @@ const comments = [
         email: "abc@123.com",
         date: "1",
         book: "Wheel of time",
+        fanList: ['hina'],
+        likes: 1,
         content: "This is absolutely fabulous!"
     },
     {
         email: "def@123.com",
         date: "1",
         book:"Harry Potter",
+        fanList: ['hina', 'penguin'],
+        likes: 2,
         content: "Well that's aright."
     },
     {
         email: "h@h.cn",
         book: "Wheel of time",
         date: "1",
+        fanList: ['hina', 'penguin'],
+        likes: 2,
         content: "This is a heart-breaking story, I love it!"
     },
     {
         email: "h@h.cn",
         book: "Harry Potter",
         date: "2",
+        fanList: ['hina', 'penguin'],
+        likes: 2,
         content: "Goooooooooooooooooood!"
     },
 ]
