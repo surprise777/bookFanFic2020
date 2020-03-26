02f7b9143b4227bbbd227b07f1e0c883bb65c85e
ShaoKan <tom19991119@gmail.com>
2020-03-26 04:24:56 -0400

    extended the expire time of cookie

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
         resave: false,
         saveUninitialized: false,
         cookie: {
-            expires: 60000,
+            expires: 600000,
             httpOnly: true
         }
     })
