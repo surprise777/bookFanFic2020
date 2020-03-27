2ab4b202ff1128a138651feb008e90bd28e9a1df
Marvel.Z <583675307@qq.com>
2020-03-26 21:13:52 -0400

    delete the section

--- client/src/components/CommentSection/commentSection.js
+++ client/src/components/CommentSection/commentSection.js
@@ -9,22 +9,22 @@ import Rating from '@material-ui/lab/Rating';
 import { TextField } from '@material-ui/core';
 import CommentSectionContent from '../../contents/commentSection';
 
-function ClickRating({rating_handler, rating}) {
+function ClickRating({ rating_handler, rating }) {
     const [setValue] = React.useState(0);
     return (
-      <div className={styles.mr2}>
-        <Box component="fieldset" borderColor="transparent">
-          <Rating
-            name="book_rating"
-            value={rating}
-            size='medium'
-            onChange={(event, newValue) => {
-              setValue(newValue);
-              rating_handler(newValue);
-            }}
-          />
-        </Box>
-      </div>
+        <div className={styles.mr2}>
+            <Box component="fieldset" borderColor="transparent">
+                <Rating
+                    name="book_rating"
+                    value={rating}
+                    size='medium'
+                    onChange={(event, newValue) => {
+                        setValue(newValue);
+                        rating_handler(newValue);
+                    }}
+                />
+            </Box>
+        </div>
     )
 }
 
@@ -63,63 +63,63 @@ class CommentSection extends React.Component {
         const comments = this.state.comment.split('\n');
         console.log(comments);
         return (
-            comments.map(c=> {
+            comments.map(c => {
                 return (
                     <div>{c}</div>
                 )
             })
-            )
+        )
     }
 
     render() {
         return (
             <Box>
                 <Container maxwidth='xl' disableGutters={true}>
-                <Box pb={2}>
-                    <Grid container>
-                        <Grid item sm={12} md={1}>
-                            <Box display='flex' justifyContent='flex-start' flexWrap='wrap' alignItems='flex-start' pt={3}>
-                                <img src={CommentSectionContent.icon} className={styles.icon} alt=''/>
-                            </Box>
-                        </Grid>
-                        <Grid item sm={12} md={11}>
-                            <Box pl={2} pt={3}>
-                                <Box pb={2}>
-                                    <TextField
-                                    fullWidth
-                                    multiline
-                                    placeholder="comments..."
-                                    value={this.state.comment}
-                                    onChange={this.commenthandler.bind(this)}
-                                    />
+                    <Box pb={2}>
+                        <Grid container>
+                            <Grid item sm={12} md={1}>
+                                <Box display='flex' justifyContent='flex-start' flexWrap='wrap' alignItems='flex-start' pt={3}>
+                                    <img src={CommentSectionContent.icon} className={styles.icon} alt='' />
                                 </Box>
-                            </Box>
-                            <Box>
-                                <Box display='flex' justifyContent='flex-end'>
-                                    <ClickRating rating_handler={this.ratinghandler.bind(this)} rating={this.state.rating}/>
-                                    <Button
-                                    onClick={this.cancelhandler.bind(this)}
-                                    >{CommentSectionContent.cancel}
-                                    </Button>
-                                    <Button color='primary'>{CommentSectionContent.comment}</Button>
+                            </Grid>
+                            <Grid item sm={12} md={11}>
+                                <Box pl={2} pt={3}>
+                                    <Box pb={2}>
+                                        <TextField
+                                            fullWidth
+                                            multiline
+                                            placeholder="comments..."
+                                            value={this.state.comment}
+                                            onChange={this.commenthandler.bind(this)}
+                                        />
+                                    </Box>
                                 </Box>
-                            </Box>
+                                <Box>
+                                    <Box display='flex' justifyContent='flex-end'>
+                                        <ClickRating rating_handler={this.ratinghandler.bind(this)} rating={this.state.rating} />
+                                        <Button
+                                            onClick={this.cancelhandler.bind(this)}
+                                        >{CommentSectionContent.cancel}
+                                        </Button>
+                                        <Button color='primary'>{CommentSectionContent.comment}</Button>
+                                    </Box>
+                                </Box>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Box>
+                    </Box>
                 </Container>
                 <Box>
-                {this.state.comments.filter(c => c.book === this.state.targetBook.brefTitle).map((comment, index) => 
-                              
-                           ( <Comment userName={this.state.allUser.filter(u => u.email===comment.email)[0].userName} 
-                            icon_url={this.state.allUser.filter(u => u.email===comment.email)[0].icon_url}
+                    {this.state.comments.filter(c => c.book === this.state.targetBook.brefTitle).map((comment, index) =>
+
+                        (<Comment userName={this.state.allUser.filter(u => u.email === comment.email)[0].userName}
+                            icon_url={this.state.allUser.filter(u => u.email === comment.email)[0].icon_url}
                             content={comment.content}
                             key={index}
                             counter={comment.likes}
                             date={comment.date}
-                            click={comment.fanList.filter(u => this.props.current.userName === u).length !== 0}/>)
-              
-                )}
+                            click={comment.fanList.filter(u => this.props.current.userName === u).length !== 0} />)
+
+                    )}
                 </Box>
             </Box>
         )
