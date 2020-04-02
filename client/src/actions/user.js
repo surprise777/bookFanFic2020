ee2d74fee36d57a7e57ee8e8b9b74b457acb7009
Marvel.Z <583675307@qq.com>
2020-04-02 12:25:31 -0400

    complete the user request

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -67,3 +67,162 @@ export const getUserById = (userId, page) => {
             console.log(error);
         });
 };
+
+export const getUsersByName = (userName, page) => {
+    const url = "/user/"+userName;
+
+    fetch(url)
+        .then(res => {
+            if (res.status === 200) {
+                return res.json();
+            } else {
+                console.log("Could not get user");
+            }
+        })
+        .then(json => {
+            page.setState({ searchUsers: json });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+export const handleUpdateSigniture = (page) => {
+
+    const url = "/user/updateSignature";
+
+    const user = page.state.current
+
+    const request = new Request(url, {
+        method: "patch",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    fetch(request)
+        .then(function (res) {
+            if (res.status === 200) {
+                console.log("update signiture Successfully.")
+            } else {
+                alert( "Signiture update Failed.")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+export const handleUpdateUserName = (page) => {
+
+    const url = "/user/updateUserName";
+
+    const user = page.state.current
+
+    const request = new Request(url, {
+        method: "patch",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    fetch(request)
+        .then(function (res) {
+            if (res.status === 200) {
+                console.log("update userName Successfully.")
+            } else {
+                alert( "UserName update Failed.")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
+export const handleUpdatePassword = (page) => {
+
+    const url = "/user/updatePassword";
+
+    const user = page.state.current
+
+    const request = new Request(url, {
+        method: "patch",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    fetch(request)
+        .then(function (res) {
+            if (res.status === 200) {
+                console.log("update password Successfully.")
+            } else {
+                alert( "Password update Failed.")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
+
+export const handleUpdateImage = (image, page) => {
+
+    const url = "/user/updateIcons";
+
+    const icons = new FormData(image)
+
+    const request = new Request(url, {
+        method: "patch",
+        body: icons,
+    });
+
+    fetch(request)
+        .then(function (res) {
+            if (res.status === 200) {
+                console.log("update icon Successfully.")
+            } else {
+                alert( "Icon update Failed.")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
+
+
+export const handleMakeAdmin = (page) => {
+
+    const url = "/user/updatePassword";
+
+    const user = page.state.current
+
+    const request = new Request(url, {
+        method: "patch",
+        body: JSON.stringify(user),
+        headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+        }
+    });
+
+    fetch(request)
+        .then(function (res) {
+            if (res.status === 200) {
+                console.log("update accountType Successfully.")
+            } else {
+                alert( "accountType update Failed.")
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+};
