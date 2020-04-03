e96b2ca9ebca7b8b75a61f808289d3aeb919fd2f
Marvel.Z <583675307@qq.com>
2020-04-03 13:55:15 -0400

    complete the searching feature

--- client/src/components/Header/header.js
+++ client/src/components/Header/header.js
@@ -19,24 +19,28 @@ class Header extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            input: this.props.state.header.input,
+            //input: this.props.state.header.input,
+            input: ""
         }
-        this.searchingChange = this.searchingChange.bind(this)
+        //this.searchingChange = this.searchingChange.bind(this)
         this.handleLogout = this.handleLogout.bind(this) 
+        // this.searchBooksByName = this.searchBooksByName.bind(this)
+        // this.searchUsersByName = this.searchUsersByName.bind(this)
+        // this.searchReviewsByName = this.searchReviewsByName.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
     }
 
-    async searchingChange(e) {
-        await this.setState({ input: e.target.value })
-        let tempState = this.props.state
-        tempState.header.input = this.state.input
-        console.log(tempState)
-        this.props.handler(tempState)
+    // async searchingChange(e) {
+    //     await this.setState({ input: e.target.value })
+    //     let tempState = this.props.state
+    //     tempState.header.input = this.state.input
+    //     console.log(tempState)
+    //     this.props.handler(tempState)
 
-    }
+    // }
 
     handleLogout(app) {
-        const url = "/users/logout";
-
+        const url = "/user/logout";
         fetch(url)
             .then(res => {
                 app.setState({
@@ -89,8 +93,132 @@ class Header extends React.Component {
             )
         }
     }
+
+    // searchingChange(e, app) {
+    //     tapp.setState({searchInput: e.target.value})
+    //     this.searchBooksByName(e.target.value, app)
+    //     this.searchUsersByName(e.target.value, app)
+    //     this.searchReviewsByName(e.target.value, app)
+    // }
+    
+    async searchingChange(e, page) {
+        await page.setState({searchInput: e.target.value})
+        console.log(this.props.app)
+    }
+
+    handleSearch(input, page){
+        const bookUrl = "/book/searchByName/"+input;
+        
+        fetch(bookUrl)
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    console.log("Could not search books");
+                }
+            })
+            .then(json => {
+                page.setState({ searchBooks: json });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        
+        const userUrl = "/user/byName/"+input;
+    
+        fetch(userUrl)
+            .then(res => {
+                    if (res.status === 200) {
+                        return res.json();
+                    } else {
+                        console.log("Could not search users");
+                    }
+                })
+                .then(json => {
+                    page.setState({ searchUsers: json });
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+
+        const reviewUrl = "/review/searchByName/"+input;
+    
+        fetch(reviewUrl)
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    console.log("Could not search reviews");
+                }
+            })
+            .then(json => {
+                page.setState({ searchReviews: json });
+            })
+            .catch(error => {
+                console.log(error);
+            });        
+            console.log(this.props.app)
+
+    };
+
+    // searchBooksByName (bookTitle, page){
+    //     const url = "/book/searchByName/"+bookTitle;
+        
+    //     fetch(url)
+    //         .then(res => {
+    //             if (res.status === 200) {
+    //                 return res.json();
+    //             } else {
+    //                 console.log("Could not search books");
+    //             }
+    //         })
+    //         .then(json => {
+    //             page.setState({ searchBooks: json });
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
+    
+    // searchUsersByName(userName, page){
+    //     const url = "/user/byName/"+userName;
+    
+    //     fetch(url)
+    //         .then(res => {
+    //             if (res.status === 200) {
+    //                 return res.json();
+    //             } else {
+    //                 console.log("Could not search users");
+    //             }
+    //         })
+    //         .then(json => {
+    //             page.setState({ searchUsers: json });
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
+
+    // searchReviewsByName (title, page){
+    //     const url = "/review/searchByName/"+title;
+    
+    //     fetch(url)
+    //         .then(res => {
+    //             if (res.status === 200) {
+    //                 return res.json();
+    //             } else {
+    //                 console.log("Could not search reviews");
+    //             }
+    //         })
+    //         .then(json => {
+    //             page.setState({ searchReviews: json });
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
+
     render() {
-        // {login_status} = this.props;
         const searchLink = window.location.pathname === RoutesMap.SearchResult.path ? RoutesMap.SearchTagResult.path : RoutesMap.SearchResult.path
         return (
             <Container className={styles.head} >
@@ -120,11 +248,11 @@ class Header extends React.Component {
                             </Hidden>
                             <Grid container item xs={8} sm={8} alignItems="center" fullWidth>
                                 <Grid container item alignItems="center" justify="center">
-                                    <TextField id="search-field" className={styles.text} placeholder={HeaderContent.searchField} fullWidth onChange={this.searchingChange}
+                                    <TextField id="search-field" className={styles.text} placeholder={HeaderContent.searchField} fullWidth onChange={(e)=>this.searchingChange(e, this.props.app)}
                                         InputProps={{
                                             endAdornment: (
                                                 <InputAdornment position="end">
-                                                    <Button> <Link className={styles.link} to={searchLink}><SearchIcon /></Link></Button>
+                                                    <Button onClick={()=>this.handleSearch(this.props.app.state.searchInput, this.props.app)}> <Link className={styles.link} to={searchLink}><SearchIcon /></Link></Button>
                                                 </InputAdornment>
                                             ),
                                         }} />
