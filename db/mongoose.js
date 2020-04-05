03fcd3bf2bd0f4b004e05fa1e5f60115dd1f7481
Marvel.Z <583675307@qq.com>
2020-04-05 08:52:14 -0400

    fix the atlas error

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 // Get the URI of the local database, or the one specified on deployment.
 const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/team19'
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+.then(() => console.log( 'Database Connected' ))
+.catch(err => console.log( err ));;
 
 module.exports = { mongoose }  // Export the active connection.
